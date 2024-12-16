46190e8b7c44749991b7cfda6ece5783d9ca2000
= <>
2024-12-16 17:23:10 -0330

    update

--- scripts/setup_alpine_vm.sh
+++ scripts/setup_alpine_vm.sh
@@ -1,4 +1,4 @@
-# bash <(curl -Ls https://raw.githubusercontent.com/truedem0n/homelab/37d9ae6c68c074e2ffd9c6cc685acec54f563689/scripts/setup_debian_vm.sh)
+# bash <(curl -Ls https://raw.githubusercontent.com/truedem0n/homelab/7f50100aad33612149058a1dffa2f1f1fd64232f/scripts/setup_alpine_vm.sh)
 
 /usr/bin/ansible-pull -U https://github.com/truedem0n/homelab.git prod/ansible/distros/alpine/base.yml
 /usr/bin/ansible-pull -U https://github.com/truedem0n/homelab.git prod/ansible/playbooks/docker.yml
