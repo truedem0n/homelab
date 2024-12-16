cb94def093200cbb728739823eb5dc86ac45b04e
= <>
2024-12-16 09:54:41 -0330

    update

--- scripts/setup_debian_vm.sh
+++ scripts/setup_debian_vm.sh
@@ -1,4 +1,4 @@
-# 
+# bash <(curl -Ls https://raw.githubusercontent.com/truedem0n/homelab/37d9ae6c68c074e2ffd9c6cc685acec54f563689/scripts/setup_debian_vm.sh)
 
 apt install git ansible -y
 /usr/bin/ansible-pull -U https://github.com/truedem0n/homelab.git prod/ansible/distros/debian/base.yml
