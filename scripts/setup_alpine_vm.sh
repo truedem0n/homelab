0dbc523bd27c83c1874fa9335d71516ccc19ea10
= <>
2024-12-16 17:37:53 -0330

    update

--- scripts/setup_alpine_vm.sh
+++ scripts/setup_alpine_vm.sh
@@ -1,4 +1,4 @@
-# bash <(curl -Ls https://raw.githubusercontent.com/truedem0n/homelab/7f50100aad33612149058a1dffa2f1f1fd64232f/scripts/setup_alpine_vm.sh)
+# bash <(curl -Ls hhttps://raw.githubusercontent.com/truedem0n/homelab/274fb8e5c0b6d96e2e592f6445e11f6f9c69d3e3/scripts/setup_alpine_vm.sh)
 
 /usr/bin/ansible-pull -U https://github.com/truedem0n/homelab.git prod/ansible/distros/alpine/base.yml
 /usr/bin/ansible-pull -U https://github.com/truedem0n/homelab.git prod/ansible/playbooks/docker.yml
--- scripts/setup_debian_vm.sh
+++ scripts/setup_debian_vm.sh
@@ -1,4 +1,4 @@
-# bash <(curl -Ls https://raw.githubusercontent.com/truedem0n/homelab/37d9ae6c68c074e2ffd9c6cc685acec54f563689/scripts/setup_debian_vm.sh)
+# bash <(curl -Ls https://raw.githubusercontent.com/truedem0n/homelab/274fb8e5c0b6d96e2e592f6445e11f6f9c69d3e3/scripts/setup_debian_vm.sh)
 
 apt install git ansible -y
 /usr/bin/ansible-pull -U https://github.com/truedem0n/homelab.git prod/ansible/distros/debian/base.yml
